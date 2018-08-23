d9196b29d3b473a2d7954ed5ed718835b7ff87a6
Victoria Luc <victoriajeniluc@gmail.com>
2018-08-23 15:48:28 -0500

    create a program to print out the name of the user

---
+++ src/_3Question.java
@@ -0,0 +1,18 @@
+/**
+ * Write a java program to accept <first name> and <last name> as command-line arguments and
+ * display message to the user:
+ * Hello <first name> <last name>, Welcome to Core Java Development Course For Beginners.
+ * If user enters only first name or last name and not both, treat the user with appropriate message,
+ * such as "Please enter both your first name and last name"
+ */
+public class _3Question {
+    public static void main(String[] args) {
+        if(args.length != 2) {
+            System.out.println("Please enter both your first name and last name");
+        } else {
+            String firstName = args[0];
+            String lastName = args[1];
+            System.out.println("Hello " + firstName + " " + lastName + ", Welcome to Core Java Development Course For Beginnners");
+        }
+    }
+}
